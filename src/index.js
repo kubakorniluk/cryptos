b98a33def2851068d3c6455e5c93b3b8e70537e5
Kuba Korniluk <kornilukkuba@gmail.com>
2021-04-18 20:46:33 +0200

    File with exports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Caesar from './ciphers/caesar';
+import Rot13 from './ciphers/rot13';
+import Atbash from './ciphers/atbash';
+import Bacon from './ciphers/bacon';
+
+export const caesar = new Caesar();
+export const rot13 = new Rot13();
+export const atbash = new Atbash();
+export const bacon = new Bacon();
